80c535efa2acf199c5d5aba8e85c87dcb87b4f3d
Antonio Seprano <antonio.seprano@docebo.com>
2020-09-04 19:55:57 +0200

    Injectiong the GamingAreaFactory into the MatchFactory

--- src/domain/factories/impl/ConcreteMatchFactory.ts
+++ src/domain/factories/impl/ConcreteMatchFactory.ts
@@ -4,24 +4,33 @@ import { Match } from "../../entities/Match";
 import { ConcreteMatch } from "../../entities/impl/ConcreteMatch";
 import { ConcreteStock } from "../../entities/impl/ConcreteStock";
 import { StdCardSerializer } from "../../domain-services/impl/StdCardSerializer";
+import { GamingAreaFactory } from "../GamingAreaFactory";
 
 export class ConcreteMatchFactory implements MatchFactory {
 
-    constructor(private idGenerator: IDGenerator) {}
+    constructor(
+        private idGenerator: IDGenerator,
+        private gamingAreaFactory: GamingAreaFactory
+    ) {}
 
     private createMatch(): ConcreteMatch {
         return new ConcreteMatch(
             new ConcreteStock(new StdCardSerializer()),
             [],
-            new StdCardSerializer()
+            new StdCardSerializer(),
+            this.gamingAreaFactory
         );
     }
 
+    private async generateMatchId(): Promise<number> {
+        return this.idGenerator.generate();
+    }
+
     public async createInitialized(): Promise<Match> {
-        return this.idGenerator.generate()
-            .then((id) => {
+        return this.generateMatchId()
+            .then((matchId) => {
                 const match = this.createMatch();
-                match.initialize(id);
+                match.initialize(matchId);
                 return match;
             });
     }
