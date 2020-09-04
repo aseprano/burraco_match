e6f4f78dde6b4f34a84dc076a2fb8f36fa8c2d3b
Antonio Seprano <antonio.seprano@docebo.com>
2020-09-05 00:47:04 +0200

    Adding some documentation to the Player interface

--- src/domain/entities/Player.ts
+++ src/domain/entities/Player.ts
@@ -5,14 +5,37 @@ import { Entity } from "./Entity";
 
 export interface Player extends Entity {
 
+    /**
+     * @throws ActionNotAllowedException
+     * @throws InsufficientCardsInStockException
+     */
     takeOneCardFromStock(): Card;
 
+    /**
+     * @throws ActionNotAllowedException
+     */
     pickUpAllCardsFromDiscardPile(): CardList;
 
+    /**
+     * @throws ActionNotAllowedException
+     * @throws CardNotOwnedException if at least one of the provided cards is not found in the player's hand
+     * @throws CannotBuildRunException if the provided card cannot be used to create a Run
+     */
     createRun(cards: CardList): Run;
 
+    /**
+     * @throws ActionNotAllowedException
+     * @throws CardNotOwnedException if at least one of the provided cards is not found in the player's hand
+     * @throws RunNotFoundException if no run with the provided id can be found in the player's gaming area
+     * @throws RunException if at least one of the provided cards cannot be added to the run
+     */
     meldCardsToRun(cards: CardList, runId: RunID): Run;
 
+    /**
+     * @throws ActionNotAllowedException
+     * @throws CardNotOwnedException if the provided card is not found in the player's hand
+     * @throws CannotDiscardCardException if the provided card cannot be discarded
+     */
     discard(card: Card): void;
 
 }
